15248b23579198d6c48f0c5c420dc39716ce44f0
Paul kafanda <paulkaf84@gmail.com>
2023-07-25 14:11:50 +0200

    ISRAEL TP

--- app/src/main/java/com/example/tpmardi/MainActivity.kt
+++ app/src/main/java/com/example/tpmardi/MainActivity.kt
@@ -1,142 +1,120 @@
 package com.example.tpmardi
 
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.tpmardi.ui.theme.TpMardiTheme
-import kotlin.math.pow
-
+import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TpMardiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    BMIApp()
-                }
-            }
+            PGCDApp()
         }
     }
 }
-
-@Composable
-fun BMIApp() {
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.LightGray)
-        ) {
-            Text(text = "BMI Calculator", modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 50.dp, bottom = 16.dp))
-        }
-        Spacer(modifier = Modifier.height(60.dp))
-        BMICalculator()
-    }
-
-}
-
-fun getBMIDescription(bmi: Float): String {
-    return when {
-        bmi <= 18.5 -> "underweight"
-        bmi > 18.5 && bmi <= 24.9 -> "healthy weight"
-        bmi > 25.0 && bmi <= 29.9 -> "overweight"
-        bmi > 30.0 && bmi <= 34.9 -> "Class I obesity"
-        bmi > 35.0 && bmi <= 39.9 -> "Class II obesity"
-        else -> "class III obesity"
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
+
+
 @Composable
-fun BMICalculator() {
-    var weight by remember { mutableStateOf("") }
-    var height by remember { mutableStateOf("") }
-    var bmi by remember { mutableStateOf(0f) }
-
+fun PGCDApp() {
+    var number1 by remember { mutableStateOf("") }
+    var number2 by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf<Triple<Int, Int, Int>?>(null) }
+    val context = LocalContext.current
+//    Spacer(modifier = Modifier.width(62.dp))
     Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        TextField(
-            value = weight,
-            onValueChange = {
-                weight = it
-                bmi = (weight.toFloatOrNull() ?: 0f) / (height.toFloatOrNull()?.pow(2) ?: 1f)
-            },
-            label = {
-                Text(text = "Your Weight")
-            },
-            colors = TextFieldDefaults.textFieldColors(containerColor = Color.LightGray),
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+    
+    ){
+        Text(text = "COEFFISCIENT DE BEZOUT")
+        OutlinedTextField(
+            value = number1,
+            onValueChange = { number1 = it },
+            label = { Text("Nombre 1") },
             modifier = Modifier.fillMaxWidth(0.8f)
         )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        TextField(
-            value = height,
-            onValueChange = {
-                height = it
-                bmi = (weight.toFloatOrNull() ?: 0f) / (height.toFloatOrNull()?.pow(2) ?: 1f)
-            },
-            label = {
-                Text(text = "Your height")
-            },
-            colors = TextFieldDefaults.textFieldColors(containerColor = Color.LightGray),
+        OutlinedTextField(
+            value = number2,
+            onValueChange = { number2 = it },
+            label = { Text("Nombre 2") },
             modifier = Modifier.fillMaxWidth(0.8f)
         )
-
-//        Spacer(modifier = Modifier.height(24.dp))
-/*
-        Button(onClick = {
-            bmi = weight.toFloat() / height.toFloat().pow(2)
-        },
-            colors = ButtonDefaults.buttonColors(Color.LightGray),
-            shape = RoundedCornerShape(8.dp),
+        Button(
+            onClick = {
+                val a = number1.toIntOrNull() ?: 0
+                val b = number2.toIntOrNull() ?: 0
+                result = calculerPGCDetCoefficients(a, b)
+            },
+            content = { Text("Calculer") },
             modifier = Modifier.fillMaxWidth(0.8f)
-        ) {
-            Text(text = "Calculate", modifier = Modifier.padding(vertical = 8.dp))
-        }*/
-
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(text = "You are ${getBMIDescription(bmi)} \nYour BMI is: $bmi")
+        )
+        result?.let { (pgcd, u, v) ->
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("PGCD: ")
+                    }
+                    append(pgcd.toString())
+                    append("\n")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Coefficients de Bézout (u et v): ")
+                    }
+                    append("$u et $v")
+                },fontSize = 18.sp,
+                modifier = Modifier.fillMaxWidth(0.8f)
+            )
+        }
     }
 }
 
-@Preview
+fun calculerPGCDetCoefficients(a: Int, b: Int): Triple<Int, Int, Int> {
+// L'algorithme d'Euclide étendu pour trouver le PGCD et les coefficients de Bézout.
+    var r0 = a
+    var r1 = b
+    var u0 = 1
+    var v0 = 0
+    var u1 = 0
+    var v1 = 1
+    while (r1 != 0) {
+        val q = r0 / r1
+        val tempR = r1
+        r1 = r0 - q * r1
+        r0 = tempR
+        val tempU = u1
+        u1 = u0 - q * u1
+        u0 = tempU
+        val tempV = v1
+        v1 = v0 - q * v1
+        v0 = tempV
+    }
+    return Triple(r0, u0, v0)
+}
+
+
+
+
+
+@Preview(showBackground = true)
 @Composable
-fun BMICalculatorPreview() {
-    BMICalculator()
+fun DefaultPreview() {
+    PGCDApp()
 }
